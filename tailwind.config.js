60a5982310a75aef4a703383c058be71b59661bc
Blanckette <adamusblanka@gmail.com>
2024-11-05 17:45:30 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    colors: {
+      cello: {
+        50: '#f4f6fb',
+        100: '#e9edf5',
+        200: '#ced9e9',
+        300: '#a3bad6',
+        400: '#7194bf',
+        500: '#4f76a8',
+        600: '#3d5e8c',
+        700: '#324b72',
+        800: '#2d425f',
+        900: '#2b3b54',
+        950: '#1b2436',
+      },
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
