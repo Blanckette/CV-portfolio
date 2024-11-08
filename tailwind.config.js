62635b1b61fa7bfd898354b60fbb46def7347f5a
Blanckette <adamusblanka@gmail.com>
2024-11-08 17:56:28 +0100

    xl 2xl size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}","./node_modules/flowbite/**/*.js"],
+  content: ["./index.html","./about.html","./text-new-cv/about.html","./**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
-    colors: {
-      cello: {
-        50: '#f4f6fb',
-        100: '#e9edf5',
-        200: '#ced9e9',
-        300: '#a3bad6',
-        400: '#7194bf',
-        500: '#4f76a8',
-        600: '#3d5e8c',
-        700: '#324b72',
-        800: '#2d425f',
-        900: '#2b3b54',
-        950: '#1b2436',
+    extend: {
+      colors: {
+        cello: {
+          50: '#f4f6fb',
+          100: '#e9edf5',
+          200: '#ced9e9',
+          300: '#a3bad6',
+          400: '#7194bf',
+          500: '#4f76a8',
+          600: '#3d5e8c',
+          700: '#324b72',
+          800: '#2d425f',
+          900: '#2b3b54',
+          950: '#1b2436',
+        },
       },
     },
-    extend: {
-  },
-}
-}
 
-// tailwind.config.js
-module.exports = {
-  // ...
-  plugins: [
-    require('tailwindcss-animated')
-  ],
-}
-
-module.exports = {
-  plugins: [require('tailwind-hamburgers')],
-}
-
-module.exports = {
-
-  plugins: [
+    plugins: [
+      require('tailwindcss-animated'),
+      require('tailwind-hamburgers'),
       require('flowbite/plugin')
-  ]
-
+    ],
+}
 }
